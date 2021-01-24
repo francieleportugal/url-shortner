9b3c31981c46cf9f1e5bdbc61a9338f652d2dd94
Franciele Portugal <franciele.portugal@gmail.com>
2021-01-24 11:48:38 -0300

    create redirect action and remove type unnecessarily

--- src/app/controllers/urlShortnerController.ts
+++ src/app/controllers/urlShortnerController.ts
@@ -2,26 +2,25 @@ import { Request, Response } from 'express';
 
 const data: Record<string, string | undefined> = {};
 
-type RequestBody = {
-    url: string,
-    name: string,
-}
-
 const urlShortnetController = {
     async create (req: Request, res: Response) {
-        const body: RequestBody = req.body;
+        const { url, name } = req.body;
 
-        data[body.name] = body.url;
-        
-        console.warn('data: ', data);
-
+        data[name] = url;
+    
         res.sendStatus(200);
     },
 
     async get (req: Request, res: Response) {
-        res.json({
-            message: 'Hello World!',
-        });
+        const { name } = req.params;
+
+        const url = data[name];
+
+        if (url) {
+            res.redirect(url);
+        } else {
+            res.sendStatus(404);
+        }
     },
 };
 

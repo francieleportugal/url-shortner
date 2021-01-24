fa0452c21022f50ab14921b9187dbeb75a23aa76
Franciele Portugal <franciele.portugal@gmail.com>
2021-01-24 10:58:36 -0300

    Create url

--- src/app/controllers/urlShortnerController.ts
+++ src/app/controllers/urlShortnerController.ts
@@ -1,10 +1,22 @@
 import { Request, Response } from 'express';
+import {ParamsDictionary} from 'express-serve-static-core'
+
+const data: any = {};
+
+type RequestBody = {
+    url: string,
+    name: string,
+}
 
 const urlShortnetController = {
     async create (req: Request, res: Response) {
-        res.json({
-            message: 'Hello World!',
-        });
+        const body: RequestBody = req.body;
+
+        data[body.name] = body.url;
+        
+        console.warn('data: ', data);
+
+        res.sendStatus(200);
     },
 
     async get (req: Request, res: Response) {

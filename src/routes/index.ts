05e35794cd689c62472e43dda51efd0b078ea802
Franciele Portugal <franciele.portugal@gmail.com>
2021-01-24 11:38:19 -0300

    add name param  in route get

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,6 +5,6 @@ import urlShortnerController from '../app/controllers/urlShortnerController';
 
 
 router.post('/', urlShortnerController.create);
-router.get('/', urlShortnerController.get);
+router.get('/:name', urlShortnerController.get);
 
 module.exports = router;

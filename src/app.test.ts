6d852649a441dc825569d3bda471a55d33ef6b4c
Franciele Portugal <franciele.portugal@gmail.com>
2021-01-24 10:58:03 -0300

    Create tests

--- src/app.test.ts
+++ src/app.test.ts
@@ -2,8 +2,18 @@ import req from "supertest";
 import app from './App';
 
 describe('app.js', () => {
-  it("[GET] /", async () => {
-    const res = await req(app).get("/");
-    expect(res.body.message).toBe('Hello World!');
+  it.only("Cadastrar url nova, ok", async () => {
+    const res = await req(app)
+      .post("/")
+      .send({ 
+        url: 'https://www.educative.io/courses/grokking-the-system-design-interview/m2ygV4E81AR' ,
+        name: 'educative',
+      })
+      .expect(200);
   });
+  it.todo('Cadastrar url com chave existente, erro');
+  it.todo('Enviar url inválida, erro');
+  it.todo('Enviar chave inválida, erro');
+  it.todo('Consultar url existente, ok');
+  it.todo('Consultar url inexistente, erro');
 });

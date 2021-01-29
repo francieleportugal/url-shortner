75c7534daa9f0256e7aca8aa4a46aeb4398587d2
Franciele Portugal <franciele.portugal@gmail.com>
2021-01-29 06:50:29 -0300

    hello world prisma

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # url-shortner
+
+## Variáveis de ambiente
+
+
+| Variável             | Descrição                                |
+| -------------------- | ---------------------------------------- |
+| `DATABASE_URL`       | URL do banco de dados                    |
+
+
+## Prisma
+```npx prisma introspect```
+
+```npx prisma introspect```
+
+```npx prisma generate```
+
+```npx prisma db push --preview-feature```
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,29 @@
+import { PrismaClient } from '@prisma/client';
 import app from './App';
 
+const prisma = new PrismaClient();
+
 const port = process.env.PORT || 3000;
+
+async function main() {
+  await prisma.url.create({
+    data: {
+      name: 'educative',
+      url: 'https://www.educative.io/courses/grokking-the-system-design-interview/m2ygV4E81AR',
+    } })
+  const all = await prisma.url.findMany()
+  console.log(all)
+}
+
+//4
+main()
+  .catch(e => {
+    throw e
+  })
+  // 5
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
 
 app.listen(port, () => {
   return console.log(`server is listening on ${port}`);
